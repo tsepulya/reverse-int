c028b8205b804dde8e840f72be672bfb39f75a9b
tsepulya <tsepeleva_anna@mail.ru>
2021-01-31 16:54:31 +0300

    fix: correct minus

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-    return Number(n.toString().split('').reverse().join(''));
+    let str = n.toString();
+    if (str[0] === "-") {
+        let str1 = str.slice(1);
+        return Number(str1.split('').reverse().join(''));
+    }
+    else {
+        return Number(str.split('').reverse().join(''));
+    }
 }
